6c3a9dba52e5049f03563161a1c545391d3dbe7b
Fred Breidt <fred.breidt@usda.gov>
2022-10-06 11:59:33 -0400

    Updated directory structure

--- Dependencies/showABD.m
+++
@@ -1,9 +0,0 @@
-function res = showABD(ABDvar)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-if ABDvar.titration_BCcurve
-   res = plotABDarea(ABDvar,1);
-else
-   res = plotABDarea(ABDvar,0);
-end
-end
